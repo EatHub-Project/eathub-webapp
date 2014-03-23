a8dc49946f5fdb9156a46338c226a9c1f92dc8d6
Sloy <rafa.vazsan@gmail.com>
2014-03-23 21:39:04 +0100

    Añadido test de ejemplo para Profile.
    Para más información sobre cómo funcionan y cómo se hacen los tests, mirar el estudio de pruebas y el mensaje del commit:
    * SHA: 2325d4702771bb4e6aaffbc8098c223b4de9f5e2

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,16 +1,28 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+# coding=utf-8
+from django.contrib.auth.models import User
 from django.test import TestCase
+from webapp.models import Location, Tastes, Profile
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ProfileTest(TestCase):
+    def setUp(self):
+        # Crea un usuario normal
+        # Como se usa el método create_user(), automáticamente se guarda en la BBDD.
+        u = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+
+        # Crea las entidades
+        loc = Location(country="Spain", city="Huerba")
+        t = Tastes(salty="5", sour="6", bitter="7", sweet="8", spicy="8")
+        p = Profile(main_language="Spanish", website="sloydev.com", sex="male", location=loc, tastes=t, user=u)
+
+        # Guarda SÓLO la entidad profile, que es la que debe ir en la colección de la bbdd. El resto son entidades embebidas.
+        p.save()
+
+    def test_recipe_well_created(self):
+        # Prueba MUY básica para obtener el objeto y mostrar algunos campos. Se deberían hacer otro tipo de pruebas unitarias
+        p = Profile.objects.get()
+        self.assertEqual(p.user.username, "john")
+        print str(p)
+        print str(p.location)
+        print str(p.user)
+        print str(p.user.email)
