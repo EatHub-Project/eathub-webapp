863579f9c718257dacfea55f881c9700b241719a
antleocar <antonioleoncarrillo@gmail.com>
2014-03-30 12:00:06 +0200

    Añadido ficheros restantes

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
 
     url(r'^$', views.main, name='main'),
     url(r'newaccount', views.new_account, name='newaccount'),
+    url(r'modifytheaccount', views.modification_account, name='modifytheaccount'),
     url(r'^login/$', views.login_user, name='login'),
     url(r'^logout/$', views.logout_user, name='logout'),
     url(r'^private/$', views.test_login_required, name='private'),
--- webapp/views.py
+++ webapp/views.py
@@ -11,6 +11,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 from webapp.forms import NewAccountForm
+from webapp.forms import ModificationAccountForm
 from django.forms.util import ErrorList
 
 
@@ -75,6 +76,67 @@ def new_account(request):
         form = NewAccountForm()
 
     return render(request, 'webapp/newaccount.html', {'form': form})
+
+
+
+def modification_account(request):
+    #TODO if user is authenticated redirect to main
+    if request.method == 'POST':
+        form = ModificationAccountForm(request.POST)
+        if form.is_valid():  # else -> render respone with the obtained form, with errors and stuff
+            # Extract the data from the form and create the User and Profile instance
+            # TODO validar que el nombre de usuario sea único
+            data = form.cleaned_data
+            username = data['username']
+            email = data['email']
+            password = data['password']
+            password_repeat = data['password_repeat']
+            # TODO comparar las contraseñas y dar error si no son iguales
+
+            display_name = data['display_name']
+            main_language = data['main_language']
+
+            additional_languages = data['additional_languages']
+            gender = data['gender']
+            country = data['country']
+            city = data['city']
+            website = data['website']
+            birth_date = data['birth_date']
+
+            if not password == password_repeat:
+                errors = form._errors.setdefault("password_repeat", ErrorList())
+                errors.append(u"Passwords don't match")
+
+            elif User.objects.filter(username=username).count():
+                errors = form._errors.setdefault("username", ErrorList())
+                errors.append(u"Username alerady taken")
+            #todo: validar email único también
+            else:
+                u = User.objects.create_user(username, email, password)
+                t = Tastes(salty=data['salty'],
+                           sour=data['sour'],
+                           bitter=data['bitter'],
+                           sweet=data['sweet'],
+                           spicy=data['spicy'])
+                p = Profile(display_name=display_name, main_language=main_language, user=u,
+                            additional_languages=additional_languages, gender=gender,
+                            location=Location(country=country, city=city), website=website,
+                            birthDate=birth_date, tastes=t)
+                #TODO capturar cualquier error de validación y meterlo como error en el formulario
+                p.clean()
+                p.save()  # TODO borrar el User si falla al guardar el perfil
+                u = authenticate(username=username, password=password)
+                login(request, u)
+                return HttpResponseRedirect(reverse('main'))  # Redirect after POST
+
+    else:
+        form = ModificationAccountForm()
+
+    return render(request, 'webapp/modifytheaccount.html', {'form': form})
+
+
+
+
 
 
 def login_user(request):
