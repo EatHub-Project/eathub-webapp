fff6830f0d3cd35446fb35a79c4accc5fefbd12e
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-04-04 19:16:23 +0200

    Corregido bug de fallo al crear usuarios con nombre de usuarios con ciertos caracteres especiales Fix #65.

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,11 +6,11 @@ urlpatterns = patterns('',
 
     url(r'^$', views.main, name='main'),
     url(r'^newaccount/$', views.new_account, name='newaccount'),
-    url(r'^profile/(?P<username>\w+)/edit/$', views.modification_account, name='profile_edit'),
     url(r'^newrecipe', views.new_recipe, name='newrecipe'),
     url(r'^login/$', views.login_user, name='login'),
     url(r'^logout/$', views.logout_user, name='logout'),
+    url(r'^profile/(?P<username>.+)/edit/$', views.modification_account, name='profile_edit'),
+    url(r'^profile/(?P<username>.+)$', views.profile, name='profile'),
     url(r'^private/$', views.test_login_required, name='private'),
     url(r'^recipe/(?P<recipe_id>\w+)/$' , views.receta, name='recipe'),
-	url(r'^profile/(?P<username>\w+)$', views.profile, name='profile'),
 )
