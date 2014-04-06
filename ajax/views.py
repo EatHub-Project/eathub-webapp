b252499faee9b6f51977cd4db7495b9a913cc6ec
David Glez <adelbzelaznog@gmail.com>
2014-04-06 23:10:53 +0200

    Implementada funcionalidad de voto.

--- ajax/views.py
+++ ajax/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.core.files import File
 from ajax.models import UploadedImage
 import json
+from webapp.models import Recipe, Vote
+from datetime import datetime
 
 
 def test(request):
@@ -16,3 +18,50 @@ def upload_picture(request):
     img = UploadedImage(image=f)
     img.save()
     return HttpResponse(json.dumps({"id": img.id, "url": img.image.url}))
+
+def vote_recipe(request):
+    # Compruebo que el usuario actual este logeado
+    ppal = request.user
+    if not ppal.is_authenticated():
+        return HttpResponse(json.dumps({"message":"No user logged in"}), status=403)
+    ppal_profile = ppal.profile.get()
+
+    # Compruebo si el usuario ya ha votado la receta previamente.
+    # Si ha votado su voto anterior se sustituye por el nuevo.
+    id = request.POST['recipe']
+    vote_type = request.POST['type']
+    recipe = Recipe.objects.get(id=id)
+    new_vote = Vote(user=ppal_profile, date=datetime.now())
+
+    for vote in recipe.positives:
+        if vote.user is ppal_profile:
+            if vote_type is u'positive':
+                return HttpResponse(json.dumps({"message": "already voted, no action is necessary"}), status=200)
+            else:
+                recipe.negatives.remove(vote)
+                recipe.positives.append(new_vote)
+
+                recipe.save()
+
+                return HttpResponse(json.dumps({"message": "OK"}), status=201)
+
+    for vote in recipe.negatives:
+        if vote.user is ppal_profile:
+            if vote_type is u'negative':
+                return HttpResponse(json.dumps({"message": "already voted, no action is necessary"}), status=200)
+            else:
+                recipe.positives.remove(vote)
+                recipe.negatives.append(new_vote)
+
+                recipe.save()
+
+                return HttpResponse(json.dumps({"message": "OK"}), status=201)
+
+    if vote_type is u'positive':
+        recipe.positives.append(new_vote)
+        recipe.save()
+    else:
+        recipe.negatives.append(new_vote)
+        recipe.save()
+
+    return HttpResponse(json.dumps({"message": "OK"}), status=201)
