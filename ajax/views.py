e6b6a5c43fc19c8375c533686c82667fc16476f0
David Glez <adelbzelaznog@gmail.com>
2014-04-07 18:04:23 +0200

    Actualizada y probada funcionalidad de votar a receta

--- ajax/views.py
+++ ajax/views.py
@@ -6,6 +6,7 @@ from ajax.models import UploadedImage
 import json
 from webapp.models import Recipe, Vote
 from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def test(request):
@@ -24,33 +25,53 @@ def vote_recipe(request):
     ppal = request.user
     if not ppal.is_authenticated():
         return HttpResponse(json.dumps({"message":"No user logged in"}), status=403)
-    ppal_profile = ppal.profile.get()
 
-    # Compruebo si el usuario ya ha votado la receta previamente.
-    # Si ha votado su voto anterior se sustituye por el nuevo.
+    # Comprobacion previa y carga de los datos de la peticion
+
+    # Compruebo que se ha especificado un id y que existe una receta con ese id
     id = request.POST['recipe']
+    if not id:
+        return HttpResponse(json.dumps({"message": "You must specify a recipe to vote by its id"}), status=400)
+
+    try:
+        recipe = Recipe.objects.get(id=id)
+    except ObjectDoesNotExist:
+        return HttpResponse(json.dumps({"message": "Invalid recipe"}), status=400)
+
+    # Compruebo que se ha especificado un tipo de voto valido y construyo un nuevo voto con el
     vote_type = request.POST['type']
-    recipe = Recipe.objects.get(id=id)
-    new_vote = Vote(user=ppal_profile, date=datetime.now())
+    if not vote_type:
+        return HttpResponse(json.dumps({"message": "You must specify a vote type (positive or negative)"}), status=400)
+    if vote_type != u'positive' and vote_type != u'negative':
+        return HttpResponse(json.dumps({"message": "You must specify a valid vote type (positive or negative)"}), status=400)
+
+    new_vote = Vote(user=ppal, date=datetime.now())
+
+    # Busco votos preexistentes del usuario a la receta y los elimino cuando sea necesario
+    found = False   #used to control when the vote has to be looked for into the lists
 
     #TODO: refactorizar! los dos bucles for son muy parecidos... se podria extraer un metodo para no repetir codigo?
-    for vote in recipe.positives:
-        if vote.user is ppal_profile:
-            if vote_type is u'positive':
-                return HttpResponse(json.dumps({"message": "already voted, no action is necessary"}), status=200)
-            else:
-                recipe.negatives.remove(vote)
-                break
+    if not found:
+        for vote in recipe.positives:
+            if vote.user == ppal:
+                if vote_type == u'positive':
+                    return HttpResponse(json.dumps({"message": "already voted, no action is necessary"}), status=200)
+                else:
+                    recipe.positives.remove(vote)
+                    found = True
+                    break
 
-    for vote in recipe.negatives:
-        if vote.user is ppal_profile:
-            if vote_type is u'negative':
-                return HttpResponse(json.dumps({"message": "already voted, no action is necessary"}), status=200)
-            else:
-                recipe.positives.remove(vote)
-                break
+    if not found:
+        for vote in recipe.negatives:
+            if vote.user == ppal:
+                if vote_type == u'negative':
+                    return HttpResponse(json.dumps({"message": "already voted, no action is necessary"}), status=200)
+                else:
+                    recipe.negatives.remove(vote)
+                    break
 
-    if vote_type is u'positive':
+    # Almaceno el nuevo voto
+    if vote_type == u'positive':
         recipe.positives.append(new_vote)
         recipe.save()
     else:
