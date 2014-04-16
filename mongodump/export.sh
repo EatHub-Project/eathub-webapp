2a52b584085e24bcd246a4d7ec64453d8b701aea
Sloy <rafa.vazsan@gmail.com>
2014-04-16 15:59:09 +0200

    Exporta también la colección de imágenes subidas.
    Hace la exportación con una query condicional, para exportar sólo las entradas correspondientes a imágenes con .persist. en el nombre.
    De esta forma sólo exporta las entradas que contienen una imagen almacenada en el repositorio, según los cambios en c62f94354529c994eccef67a3daf906233cc0f6a
    
    Actualizo los datos exportados para incluír una imagen de perfil con el nuevo sistema de exportación.

--- mongodump/export.sh
+++ mongodump/export.sh
@@ -5,3 +5,9 @@ do
   cat $OUT_DIR/$col.json.tmp | python -m json.tool > $OUT_DIR/$col.json
   rm $OUT_DIR/$col.json.tmp
 done
+
+# La colección de imágenes la exporta con una query condicional
+col="ajax_uploadedimage"
+mongoexport -d eathub -c $col -o $OUT_DIR/$col.json.tmp --jsonArray -q '{image: {$regex: ".+\.persist\..+"}}'
+cat $OUT_DIR/$col.json.tmp | python -m json.tool > $OUT_DIR/$col.json
+rm $OUT_DIR/$col.json.tmp
--- mongodump/import.sh
+++ mongodump/import.sh
@@ -1,5 +1,5 @@
 OUT_DIR="exported_json"
-for col in "webapp_recipe" "webapp_profile" "auth_user"
+for col in "webapp_recipe" "webapp_profile" "auth_user" "ajax_uploadedimage"
 do
   mongoimport -d eathub -c $col --file $OUT_DIR/$col.json --drop --jsonArray
 done
