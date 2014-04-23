30f8090b91345c0135a69a35c3839981843bdcf4
serrodcal <sergiorodriguezcalvo@gmail.com>
2014-04-23 22:53:03 +0200

    Corregidos los ficheros urls.py, tanto de django, como de la aplicacion.

--- eathub/urls.py
+++ eathub/urls.py
@@ -17,7 +17,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
-
+    
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^', include('webapp.urls')),
     url(r'^ajax/', include('ajax.urls')),
 
