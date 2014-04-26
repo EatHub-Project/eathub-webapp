2516088b081476a6e660033ee5684ef2ae34da81
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-04-26 19:52:58 +0200

    Actualizado datos de prueba asi como los ejecutables de importación y exportacion para incluir estos documentos.

--- mongodump/export.bat
+++ mongodump/export.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set OUT_DIR=exported_json
-for %%x in (webapp_recipe, webapp_profile, auth_user) do (
+for %%x in (webapp_recipe, webapp_profile, auth_user, webapp_country, webapp_food_type, webapp_language, webapp_special_condition, webapp_temporality) do (
   mongoexport -d eathub -c %%x -o %OUT_DIR%/%%x.json.tmp --jsonArray
   python -m json.tool %OUT_DIR%/%%x.json.tmp>%OUT_DIR%/%%x.json
   rm %OUT_DIR%/%%x.json.tmp
