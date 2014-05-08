6edf343d2bd900ca23b9829b70e8baf5a9f35442
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-05-08 13:11:07 +0200

    Modificada la creación de los indice, se mapean todos los campos y por defecto en idioma español

--- webapp/management/commands/createindex.py
+++ webapp/management/commands/createindex.py
@@ -7,29 +7,8 @@ class Command(NoArgsCommand):
     def handle(self, **options):
         db = MongoClient()
 
-        db.eathub.webapp_recipe.ensure_index(
-            [
-                 ('title', 'text'),
-                 ('description', 'text'),
-             ],
-            name="search_index_recipe",
-            weights={
-                'title':100,
-                'description':25
-            }
-        )
+        db.eathub.webapp_recipe.ensure_index([("$**", 'text',)], name="recipe_text", default_language = "spanish")
+        db.eathub.webapp_profile.ensure_index([("$**", 'text',)], name="profile_text", default_language = "spanish")
 
-        db.eathub.webapp_profile.ensure_index(
-            [
-                ('display_name', 'text'),
-                ('location', 'text'),
-            ],
-            name="search_index_profile",
-            weights={
-                'display_name':100,
-                'location':25
-            }
-        )
-
-        self.stdout.write('yeah nigga, ya es fuckerday motherfraideis!')
+        self.stdout.write('Indices creados')
 
