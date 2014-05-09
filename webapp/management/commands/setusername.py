d3dccc29586a07a9e54a8ad6becfd978f7b31738
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-05-09 18:40:54 +0200

    En el comando setusername se muestra el id del usuario procesado por si falla

--- webapp/management/commands/setusername.py
+++ webapp/management/commands/setusername.py
@@ -11,6 +11,8 @@ class Command(NoArgsCommand):
         usuarios = User.objects.all()
         self.stdout.write('There are "%s" users' % usuarios.__len__())
         for u in usuarios:
+            self.stdout.write('The user_id is "%s" ' % u.id)
+            #self.stdout.write('and de profile id "%s"' % u.profile.id)
             p = u.profile.get()
             #self.stdout.write('The username is "%s"' % u.username)
             p.username=u.username
