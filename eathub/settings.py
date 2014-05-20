99bb4c55eac51c2edf265453a060ff7e3e99463d
Sloy <rafa.vazsan@gmail.com>
2014-05-20 21:57:28 +0200

    Corregido activar debug / ssl en remoto y desactivar en local
    Usa una variable de entorno para detectar cuándo está en remoto y cuándo en local. (La variable tiene como nombre DEBUG, y se considera activada si contiene la cadena "True" o si no existe.
    Añade "*" a la lista de hosts permitidos. Es un poco bestia y habría que limitarlo a nuestros hosts, pero de momento se deja así.
    Desactivado SSLify cuando se está en debug, para que no falle por falta de certificado ssl al redirigir a https.

--- eathub/settings.py
+++ eathub/settings.py
@@ -6,7 +6,12 @@ from django.contrib.messages import constants as message_constants
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-DEBUG = True
+
+if os.getenv('DEBUG', "True") == "True":
+    DEBUG = True
+else:
+    DEBUG = False
+
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -37,7 +42,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -255,3 +260,7 @@ REST_FRAMEWORK = {
 
     'DATETIME_FORMAT': ['%s']
 }
+
+# Disable SSLify if DEBUG is enabled.
+if DEBUG:
+    SSLIFY_DISABLE = True
