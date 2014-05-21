ec21b2b1173de82e7c20c49640b2ee2a47e3e12f
Sergio Garcia Alonso <sga.vmp@gmail.com>
2014-05-21 17:12:20 +0200

    Corregido indices para funcionar en Heroku y en local.

--- webapp/management/commands/createindex.py
+++ webapp/management/commands/createindex.py
@@ -1,11 +1,23 @@
+import os
 from pymongo import *
+from urlparse import urlparse
 from django.core.management.base import NoArgsCommand, CommandError
 
 class Command(NoArgsCommand):
     help = 'Create index'
 
     def handle(self, **options):
-        db = MongoClient()
+        MONGO_URL = os.environ.get('MONGOHQ_URL')
+
+        if MONGO_URL:
+            # Get a connection
+            conn = pymongo.Connection(MONGO_URL)
+
+            # Get the database
+            db = conn[urlparse(MONGO_URL).path[1:]]
+        else:
+            # Not on an app with the MongoHQ add-on, do some localhost action
+            db = MongoClient()
 
         db.eathub.webapp_recipe.ensure_index([("$**", 'text',)], name="recipe_text", default_language = "spanish")
         db.eathub.webapp_profile.ensure_index([("$**", 'text',)], name="profile_text", default_language = "spanish")
